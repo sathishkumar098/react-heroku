689024ba9598eebd21844b2a2ca868c4f5836afb
sathishkumar098 <61049352+sathishkumar098@users.noreply.github.com>
2021-06-19 13:09:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Sathish Kumar<h1>
+        <h1>Sathish Kumar</h1>
       </header>
     </div>
   );
